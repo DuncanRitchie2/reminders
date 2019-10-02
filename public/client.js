49fc8929ef8c387b16293e5cebe902d5d5d5a9be
CN0103 <cn0103@CN103.local>
2019-10-02 15:40:00 +0100

    index page with splash and style

--- public/client.js
+++ public/client.js
@@ -1,6 +1,11 @@
 const In=document.getElementById("in");
 const Up=document.getElementById("up");
+const In1=document.getElementById("in1");
+const Up1=document.getElementById("up1");
 const splash=document.getElementById('sign-cont');
+
+signIn.style.display="none";
+signUp.style.display="none";
 
 In.addEventListener("click",()=>{
     signIn.style.display="flex";
@@ -9,6 +14,18 @@ In.addEventListener("click",()=>{
 })
 
 Up.addEventListener("click",()=>{
+    signUp.style.display="flex";
+    signIn.style.display="none";
+    splash.style.display="none";
+})
+
+In1.addEventListener("click",()=>{
+    signIn.style.display="flex";
+    signUp.style.display="none";
+    splash.style.display="none";
+})
+
+Up1.addEventListener("click",()=>{
     signUp.style.display="flex";
     signIn.style.display="none";
     splash.style.display="none";
