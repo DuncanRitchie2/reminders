4e1d589a94f7d8d56e26d6dff57b14aa6facc6d4
CN0103 <cn0103@CN103.local>
2019-10-07 17:22:30 +0100

    finished

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { promisify } = require('util')
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "password",
+    password: "StEfAn20192792",
     database: "reminder_app"
 })
 
