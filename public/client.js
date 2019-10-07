96250a4ee7cde15618c2e1a549dd27b9b8d67ace
Duncan Ritchie <duncanritchie@btopenworld.com>
2019-10-07 11:12:50 +0100

    Sign-in redirects to dashboard

--- public/client.js
+++ public/client.js
@@ -114,10 +114,11 @@ const signIn = async () => {
     console.log(`sign in user has id ${data.id}`)
     // should get a user_id back
 
-
     // We need to enter user_id into local storage so that dashboard functions can access it.
     localStorage.setItem('reminders_user_id', data.id);
     console.log("Local storage has "+localStorage.getItem('reminders_user_id'))
+
+    location.pathname = "/dashboard.html"
 }
 
 if (signInButton) {
